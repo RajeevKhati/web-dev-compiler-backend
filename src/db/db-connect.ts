486a1ad4bee760a1ab78730a5ffd55427eb0346e
RajeevKhati <rajeev.khati@nagarro.com>
2024-02-20 07:49:31 +0530

    feat(mongo): create schema, model, controller for fullCode

---
+++ src/db/db-connect.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {
+      dbName: "wd-compiler",
+    });
+    console.log("Connection established..");
+  } catch (error) {
+    console.log("Connection to database failed", error);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { dbConnect } from "./db/db-connect";
+import { compilerRouter } from "./routes/compiler-router";
 
+// Load environment variables
+config();
+
+// Initialize Express app
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Database connection
+dbConnect();
+
+// Routes
+app.use("/compiler", compilerRouter);
+
+// Start the server
 app.listen(3000, () => {
-  console.log("listening on 3000...");
+  console.log("Server is listening on port 3000...");
 });
